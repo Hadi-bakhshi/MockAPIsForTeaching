9afea15f6ed8709c799b634e03a8bebfd93daf13
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-08-02 13:18:10 +0330

    chore: change namespace name

--- Infrastructure/GlobalExceptionHandler.cs
+++ Infrastructure/GlobalExceptionHandler.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
-namespace WebApplication1.Infrastructure;
+namespace MockAPI.Infrastructure;
 
 public class GlobalExceptionHandler : IExceptionHandler
 {
